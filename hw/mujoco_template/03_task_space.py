eb4515ad6c3a5d3f31f1f80fbd78242e387ec236
simeon <simkaned@gmail.com>
2024-12-10 00:43:49 +0300

    added conversion to SE3 in example

--- hw/mujoco_template/03_task_space.py
+++ hw/mujoco_template/03_task_space.py
@@ -25,6 +25,8 @@ import numpy as np
 from simulator import Simulator
 from pathlib import Path
 from typing import Dict
+import os 
+import pinocchio as pin
 
 def task_space_controller(q: np.ndarray, dq: np.ndarray, t: float, desired: Dict) -> np.ndarray:
     """Example task space controller."""
@@ -32,14 +34,20 @@ def task_space_controller(q: np.ndarray, dq: np.ndarray, t: float, desired: Dict
     
     kp = np.array([1000, 1000, 1000, 10, 10, 0.1])
     kd = np.array([200, 200, 200, 2, 2, 0.01])
-    q0 = np.array([-1.4, -1.3, 1., 0, 0, 0])
+    q0 = np.array([0.0, -1.3, 1., 0, 0, 0])
     tau = kp * (q0 - q) - kd * dq
     
-    # The joint space controller is above, but you can use the task space controller instead
-    # The trajectory can be hardcoded here or generated based on the desired task
-    desired_position = desired['pos'] # [x_des, y_des, z_des]
-    desired_quaternion = desired['quat'] # [w_des, x_des, y_des, z_des]
-    print(desired_position, desired_quaternion)
+    # Convert desired pose to SE3
+    desired_position = desired['pos']
+    desired_quaternion = desired['quat'] # [w, x, y, z] in MuJoCo format
+    desired_quaternion_pin = np.array([*desired_quaternion[1:], desired_quaternion[0]]) # Convert to [x,y,z,w] for Pinocchio
+    # Convert to pose and SE3
+    desired_pose = np.concatenate([desired_position, desired_quaternion_pin])
+    desired_se3 = pin.XYZQUATToSE3(desired_pose)
+    print(desired_se3)
+    # Get end-effector frame
+    ee_frame_id = model.getFrameId("end_effector")
+    
     return tau
 
 def main():
@@ -61,4 +69,8 @@ def main():
     sim.run(time_limit=10.0)
 
 if __name__ == "__main__":
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    xml_path = os.path.join(current_dir, "robots/universal_robots_ur5e/ur5e.xml")
+    model = pin.buildModelFromMJCF(xml_path)
+    data = model.createData()
     main() 
